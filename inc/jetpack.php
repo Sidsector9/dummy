c17b08277d1e60d970b9ba0df4b1810d287d2cc6
Sidsector9 <siddharth.thevaril@gmail.com>
2017-10-30 10:00:43 +0530

    AC#15 Added underscore starter theme

---
+++ inc/jetpack.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package The_Keynote
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
+ */
+function thekeynote_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'thekeynote_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support( 'jetpack-content-options', array(
+		'post-details' => array(
+			'stylesheet' => 'thekeynote-style',
+			'date'       => '.posted-on',
+			'categories' => '.cat-links',
+			'tags'       => '.tags-links',
+			'author'     => '.byline',
+			'comment'    => '.comments-link',
+		),
+	) );
+}
+add_action( 'after_setup_theme', 'thekeynote_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function thekeynote_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
